0181827f1465445e532290d95fb74e16b21df1ff
Justin Durack <justin@magicnotebook.com>
2013-03-15 11:41:41 -0700

    Added "app running" log

--- mikeymail.js
+++ mikeymail.js
@@ -2,6 +2,7 @@ var serverCommon = process.env.SERVER_COMMON;
 
 var winston = require (serverCommon + '/lib/winstonWrapper').winston;
 winston.logBreak();
+winston.doInfo('mikeymail app running...');
 
 var constants = require ('./constants'),
     memwatch = require('memwatch'),
