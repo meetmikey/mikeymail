8ff325a4fa3b7a0c465fddd75dd894ab02a1e2df
Sagar Mehta <mehta.sagarv@gmail.com>
2013-04-06 14:45:07 -0700

    allow a resume

--- constants.js
+++ constants.js
@@ -31,7 +31,7 @@ define('INDEX_NAME', 'document_index');
 // how many of each job to do per node
 define('MAX_DOWNLOAD_JOBS', 4);
 define('MAX_UPDATE_JOBS', 20);
-define('MAX_RESUME_DOWNLOAD_JOBS', 0);
+define('MAX_RESUME_DOWNLOAD_JOBS', 1);
 
 
 // polling intervals
