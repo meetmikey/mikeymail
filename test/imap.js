5cf6e5303c13b7be277395e047ef60a90685d68a
Sagar Mehta <mehta.sagarv@gmail.com>
2013-08-08 16:34:33 -0700

    fixed bug when both GMail and Google Mail boxes are present in 'All Mail' search

--- test/imap.js
+++ test/imap.js
@@ -12,43 +12,8 @@ var initActions = [
   appInitUtils.CONNECT_MONGO
 ];
 
-
-var userInfo = {
-  "shortId" : "1",
-  "googleID" : "115882407960585095714",
-  "accessHash" : "0aa51b7b174318a6437c35a6401238a34c43e10df73eb5b9d86c9599530aff5ef76370a8885119e68bf0ce2f91eef4500ce9b3330cd432b97aee5ba73196c651",
-  "displayName" : "Sagar Mehta",
-  "firstName" : "Sagar",
-  "lastName" : "Mehta",
-  "email" : "sagar@mikeyteam.com",
-  "locale" : "en",
-  "hostedDomain" : "mikeyteam.com",
-  "expiresAt" : "2013-08-05T19:10:43.999Z",
-  "symHash" : "1e9d392306eb25b161f4ea41172821b8b035d98aeaa462a8f400266bc64b91bb0437d5f763703b90052306326a02ad2335fda3d43b58b4617fbd3ad9f5e7d4d5",
-  "symSalt" : "a954e94e3302046f",
-  "asymHash" : "$2a$08$vA1D6sYxvHzHlg9yVzmCQuu20VGMcUwMiSIdj3fbH4hGl4FJTzVtK",
-  "asymSalt" : "$2a$08$vA1D6sYxvHzHlg9yVzmCQu",
-  "_id" : "51ffeaa4820a4a7a1700000a",
-  "lastResumeJobEndDate" : "2013-08-05T18:10:44.024Z",
-  "billingPlan" : "free",
-  "isPremium" : false,
-  "daysLimit" : 90,
-  "allMailOnboardAttempts" : 0,
-  "minMailDate" : "2013-08-05T18:10:44.024Z",
-  "minMRProcessedDate" : "2013-08-05T18:10:44.024Z",
-  "minProcessedDate" : "2013-08-05T18:10:44.024Z",
-  "timestamp" : "2013-08-05T18:10:44.024Z",
-  "invalidToken" : false,
-  "gmailScrapeRequested" : true,
-  "__v" : 0
-}
-
-
-
 appInitUtils.initApp( 'imap', initActions, null, function() {
-  UserModel.findById ("52002b49a108c2a729000010", function (err, userInfo) {
-
-    console.log (userInfo.accessToken)
+  UserModel.findById ("52022a089d3d2f265a000884", function (err, userInfo) {
 
     var xoauthParams = daemonUtils.getXOauthParams (userInfo);
     var xoauth2gen = xoauth2.createXOAuth2Generator(xoauthParams);
